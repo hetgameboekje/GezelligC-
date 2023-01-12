288def18b7de289b170aeb3093989715a8f58794
HetGameBoekje <514527@vistacollege.nl>
2023-01-12 15:36:52 +0100

    IF ELSE

--- Program.cs
+++ Program.cs
@@ -4,8 +4,23 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args){
-            Console.Writeline("HelloWorld");
+        static void Main(string[] args)
+        {
+            int i = 10;
+            int j = 4;
+
+            if (i < j)
+            {
+                Console.WriteLine("i is less than j");
+            }
+
+            if (i > j)
+            {
+                Console.WriteLine("i is greater than j");
+            }
+
+
+
         }
     }
 }
